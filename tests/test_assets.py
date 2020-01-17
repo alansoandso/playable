36ad105ebb411b0f3550262985b4f8f2ca1b044a
Alan So <alan.so@sky.uk>
2020-01-17 11:55:22 +0000

    Add live logging

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -1,9 +1,12 @@
 from unittest.mock import patch, mock_open
 from tool.assets import Assets
+import logging
+LOGGER = logging.getLogger(__name__)
 
 
 @patch('builtins.open', mock_open(read_data='{"default":{"The Firm": "crid1", "Joker": "crid2"}}'))
 def test_load_crids():
+    LOGGER.info('Log some info to stdout')
     assert Assets.load_crids() == {"The Firm": "crid1", "Joker": "crid2"}
     # noinspection PyUnresolvedReferences
     open.assert_called_once()
